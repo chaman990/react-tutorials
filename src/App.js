ec34fb7b792f30febf10c25c61f103b5ad3f3fa9
Sspl Chaman <sspl.chaman.intern@gmail.com>
2020-10-14 15:33:30 +0530

    updpate

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React from 'react';
-import Login from './components/Login';
-import Register from './components/Register';
-import Dashboard from './components/Dashboard';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// for dashboard
-import { useRoutes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core';
-import GlobalStyles from './components/GlobalStyles';
-import './mixins/chartjs';
-import theme from './theme';
-import routes from './routes';
-
-
-
-export const apiURL = "http://localhost:8080/react-tutorials/src/api/";
-
-export default class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  render() {
-     const routing = useRoutes(routes);
-    return (
-      <Router>
-       <Routes>
-         <Route exact path="/dashboard">
-             <ThemeProvider theme={theme}>
-              <GlobalStyles />
-              {routing}
-            </ThemeProvider>
-         </Route>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Routes>
-      </Router>
-    );
-  }
+const App = (props) =>{
+  return (
+    <>Tes</>
+  )
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import ToastrContainer from 'react-toastr-basic'
-
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <ToastrContainer />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
-// toastr js basic link : - https://github.com/kimwandev/react-toastr-basic
